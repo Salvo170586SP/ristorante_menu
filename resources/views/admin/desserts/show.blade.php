2db7663d325d6a5535033c375d0ca19abdd4442c
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2023-06-14 16:23:10 +0200

    added desserts table crud

---
+++ resources/views/admin/desserts/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-12 d-flex justify-content-between align-items-center py-5">
+            <h1>Dettaglio Dessert</h1>
+            <a href="{{ route('admin.desserts.index') }}" class="btn btn-secondary shadow">Torna alla lista</a>
+        </div>
+
+        <div class="col-12">
+            <div class="card shadow">
+                <div class="card-body">
+                    <h3 class="card-title">Nome: {{ $dessert->name }}</h3>
+                    <h4>Prezzo: @if($dessert->price) €{{ $dessert->price }} @else -- @endif</h4>
+                    <p class="card-text">Descrizione: @if($dessert->description) {{ $dessert->description }} @else -- @endif</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
