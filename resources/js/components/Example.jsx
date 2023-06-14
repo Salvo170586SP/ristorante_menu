62c8e7ccfb2a6e5ae8b3d079f84fb8031d5f8215
Nicolas Maranzano <>
2023-06-14 10:10:43 +0200

    inizializzato menù + installate dipendenze

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -1,19 +1,130 @@
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Container from '@mui/material/Container';
 import ReactDOM from 'react-dom/client';
+
+const items = [
+    {
+      id: 1,
+      title: 'buttermilk pancakes',
+      category: 'breakfast',
+      price: 15.99,
+      img: './images/item-1.jpeg',
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+      id: 2,
+      title: 'diner double',
+      category: 'lunch',
+      price: 13.99,
+      img: './images/item-2.jpeg',
+      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    },
+    {
+      id: 3,
+      title: 'godzilla milkshake',
+      category: 'shakes',
+      price: 6.99,
+      img: './images/item-3.jpeg',
+      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    },
+    {
+      id: 4,
+      title: 'country delight',
+      category: 'breakfast',
+      price: 20.99,
+      img: './images/item-4.jpeg',
+      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    },
+    {
+      id: 5,
+      title: 'egg attack',
+      category: 'lunch',
+      price: 22.99,
+      img: './images/item-5.jpeg',
+      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    },
+    {
+      id: 6,
+      title: 'oreo dream',
+      category: 'shakes',
+      price: 18.99,
+      img: './images/item-6.jpeg',
+      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    },
+    {
+      id: 7,
+      title: 'bacon overflow',
+      category: 'breakfast',
+      price: 8.99,
+      img: './images/item-7.jpeg',
+      desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    },
+    {
+      id: 8,
+      title: 'american classic',
+      category: 'lunch',
+      price: 12.99,
+      img: './images/item-8.jpeg',
+      desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    },
+    {
+      id: 9,
+      title: 'quarantine buddy',
+      category: 'shakes',
+      price: 16.99,
+      img: './images/item-9.jpeg',
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+  ];
 
 function Example() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">GUEST</div>
+        <>
+    <AppBar position="static" sx={{ backgroundColor: 'white !important'}}>
+        <Container maxWidth="xl">
+            <Toolbar disableGutters className='flex justify-center'>
+                <img width="300" height="300" src={"https://d1csarkz8obe9u.cloudfront.net/posterpreviews/restaurant-logo-design-template-9c8fdc997056656a46248a9f5735d53f_screen.jpg?ts=1625564746"} alt='logo ristorante'/>
+            </Toolbar>
+        </Container>
+    </AppBar>
 
-                        <div className="card-body">PAGINA IN COSTRUZIONE</div>
-                    </div>
-                </div>
+    <div className="section-center pt-4">
+    {items.map((menuItem) => {
+    const { id, title, img, desc, price, category } = menuItem;
+    return (
+        <Accordion key={id}>
+        <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+        >
+            <Typography>{category}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+        <article className="menu-item">
+            {/* <img src={img} alt={title} className="photo" /> */}
+            <div className="item-info">
+                <header>
+                <h4>{title}</h4>
+                <h4 className="price">${price}</h4>
+                </header>
+                <p className="item-text">{desc}</p>
             </div>
-        </div>
+            </article>
+        </AccordionDetails>
+    </Accordion>
+    
+    );
+    })}
+    </div>
+</>
     );
 }
 
