39e9be90e66e96f306ccbf1e6f9eb9942f1f52d5
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2023-06-21 13:36:04 +0200

    delete comments

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -28,56 +28,6 @@
                                 <div class="fw-bold"> <a class="btn btn-secondary" href="{{ route('admin.products.index') }}">Products</a></div>
                                 <span class="badge bg-secondary rounded-pill">{{ count($products) }}</span>
                             </li>
-                          {{--   <li class="list-group-item d-flex justify-content-between align-items-start">
-                                <div class="fw-bold"> <a class="btn btn-secondary" href="{{ route('admin.aperitifs.index') }}">Aperitifs</a></div>
-                                <span class="badge bg-secondary rounded-pill">{{ count($aperitifs) }}</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-start">
-                                <div class="fw-bold"><a class="btn btn-secondary" href="{{ route('admin.desserts.index') }}">Desserts</a></div>
-                                <span class="badge bg-secondary rounded-pill">{{ count($desserts) }}</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-start">
-                                <div class="fw-bold"><a class="btn btn-secondary" href="{{ route('admin.long_drinks.index') }}">Long Drinks</a> </div>
-                                <span class="badge bg-secondary rounded-pill">{{ count($longDrinks) }}</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-start">
-                                <div class="fw-bold"><a class="btn btn-secondary" href="{{ route('admin.special_long_drinks.index') }}">Special Long Drinks</a>
-                                </div>
-                                <span class="badge bg-secondary rounded-pill">{{ count($specialLongDrinks) }}</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-start">
-                                <div class="fw-bold"><a class="btn btn-secondary" href="{{ route('admin.international_long_drinks.index') }}">International Long Drinks</a>
-                                </div>
-                                <span class="badge bg-secondary rounded-pill">{{ count($internationalLongDrinks) }}</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-start">
-                                <div class="fw-bold"><a class="btn btn-secondary" href="{{ route('admin.white_wines.index') }}">White Wines</a></div>
-                                <span class="badge bg-secondary rounded-pill">{{ count($whiteWines) }}</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-start">
-                                <div class="fw-bold"><a class="btn btn-secondary" href="{{ route('admin.red_wines.index') }}">Red Wines</a></div>
-                                <span class="badge bg-secondary rounded-pill">{{ count($redWines) }}</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-start">
-                                <div class="fw-bold"><a class="btn btn-secondary" href="{{ route('admin.beers.index') }}">Beers</a></div>
-                                <span class="badge bg-secondary rounded-pill">{{ count($beers) }}</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-start">
-                                <div class="fw-bold"><a class="btn btn-secondary" href="{{ route('admin.bitter_drinks.index') }}">Amari</a></div>
-                                <span class="badge bg-secondary rounded-pill">{{ count($bitterDrinks) }}</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-start">
-                                <div class="fw-bold"><a class="btn btn-secondary" href="{{ route('admin.whiskies.index') }}">Whiskies</a></div>
-                                <span class="badge bg-secondary rounded-pill">{{ count($whiskies) }}</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-start">
-                                <div class="fw-bold"><a class="btn btn-secondary" href="{{ route('admin.soft_drinks.index') }}">Soft Drinks</a></div>
-                                <span class="badge bg-secondary rounded-pill">{{ count($softDrinks) }}</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-start">
-                                <div class="fw-bold"><a class="btn btn-secondary" href="{{ route('admin.bottles.index') }}">Bottles</a></div>
-                                <span class="badge bg-secondary rounded-pill">{{ count($bottles) }}</span>
-                            </li> --}}
                         </ol>
                     </div>
                 </div>
