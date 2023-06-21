e491780286fe1290144a2f4f8c3a3b031e64688f
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2023-06-21 18:58:36 +0200

    fixed api categories

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::all();
+        $categories = Category::with('products')->get();
  
         return response()->json($categories);
     }
