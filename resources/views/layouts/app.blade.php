4420546ebf7bcf67d95466e20afc5e4a20b2e9cc
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2023-07-13 13:04:43 +0200

    fix logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
             <div class="container">
                 {{-- <a class="navbar-brand" href="{{ url('/') }}"> --}}
                     {{-- {{ config('app.name', 'Laravel') }} --}}
-                    <img width="50" src="{{ asset('assets/images/logo.jpg') }}" alt="">
+                    <img width="50" class="border bg-dark rounded p-1" src="http://www.plantapalermo.it/immagini/logo.png" alt="logo">
               {{--   </a> --}}
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
